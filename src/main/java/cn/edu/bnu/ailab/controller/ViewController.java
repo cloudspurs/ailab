582d44a0e50f5a2ba2533a1ab8ba27853da69eef
mengqinggang <mqg@email.com>
2018-08-08 18:56:43 +0800

    0808

--- src/main/java/cn/edu/bnu/ailab/controller/ViewController.java
+++ src/main/java/cn/edu/bnu/ailab/controller/ViewController.java
@@ -1,42 +1,16 @@
 package cn.edu.bnu.ailab.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(value = "/ailab")
 public class ViewController {
 	
-	@RequestMapping(value = "/index")
-	public String index() { return "index"; }
-
-	@RequestMapping(value = "/members")
-	public String members() { return "members"; }
-
-	@RequestMapping(value = "/papers")
-	public String papers() { return "papers"; }
+	@RequestMapping(value = "/")
+	public String home() { return "index"; } 
 	
-	@RequestMapping(value = "/paper1")
-	public String papers1() { return "paper1"; }
-	
-	@RequestMapping(value = "/news")
-	public String news() { return "news"; }
-
-	@RequestMapping(value = "/news1")
-	public String news1() { return "news1"; }
-
-	@RequestMapping(value = "/academic")
-	public String academic() { return "academic"; }
-
-	@RequestMapping(value = "/academic1")
-	public String academic1() { return "academic1"; }
-
-	@RequestMapping(value = "/recruit")
-	public String recruit() { return "recruit"; }
-
-	@RequestMapping(value = "/about")
-	public String about() { return "about"; }
-
-	@RequestMapping(value = "/tmp")
-	public String master() { return "tmp"; }
+	@RequestMapping(value = "/{url}")
+	public String URL(@PathVariable("url") String url) { return url; }
 }
